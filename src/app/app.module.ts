29e8c1c4ed13c50e05441583fddfab8ba71663cc
tairamos <tairamos.dev@gmail.com>
2021-08-30 23:21:33 -0300

    Implementando o cadastro de empresas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastrarEmpresaComponent } from './cadastrar-empresa/cadastrar-empresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastrarEmpresaComponent
   ],
   imports: [
     BrowserModule,
