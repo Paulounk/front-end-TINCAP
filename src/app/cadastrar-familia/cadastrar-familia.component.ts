d5c081bdfe22aa2ea8136adf3e3b70c71c4e1dc9
Jose-Araujo <jose.vinicius450@gmail.com>
2021-09-03 17:55:34 -0300

    Implementando função cadastrar

--- src/app/cadastrar-familia/cadastrar-familia.component.ts
+++ src/app/cadastrar-familia/cadastrar-familia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar-familia',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarFamiliaComponent implements OnInit {
 
-  constructor() { }
+  user: Usuario: new Usuario()
+confrimarSenha: string
 
-  ngOnInit(): void {
+constructor(
+  private authService: AuthService,
+  private router: Router
+) { }
+
+ngOnInit(){
+  window.scroll(0, 0)
+}
+
+confirmeSenha(event: any){
+  this.confirmarSenha = event.target.value
+}
+
+cadastrar(){
+
+  if (this.user.nome.length < 5) {
+    alert('O usuário deve conter no mínimo 8 caracteres.')
   }
+  if (this.user.usuario.indexOf('@') == -1 || this.user.usuario.indexOf('.') == -1) {
+    alert('O usuário deve ser um email (e.g. usuario@usuario.com)')
+  }
+
+  if (this.user.senha.length < 8) {
+    alert('A senha deve conter no mínimo 8 dígitos.')
+  } else if (this.user.senha != this.confirmarSenha) {
+    alert('As senhas informadas estão diferentes!')
+  } else {
+    this.authService.cadastrar(this.user).subscribe((resp: User) => {
+      this.user = resp
+      this.router.navigate(['/entrar'])
+      alert('Usuário cadastrado com sucesso')
+    })
+  }
+}
+
+
 
 }
