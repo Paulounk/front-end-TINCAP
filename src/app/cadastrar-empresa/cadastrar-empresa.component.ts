7d35473ece8dc8e35dd998cad627108ab9878843
tairamos <tairamos.dev@gmail.com>
2021-09-08 11:36:41 -0300

    Merge da branch cadastrar empresa

--- src/app/cadastrar-empresa/cadastrar-empresa.component.ts
+++ src/app/cadastrar-empresa/cadastrar-empresa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar-empresa',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarEmpresaComponent implements OnInit {
 
-  constructor() { }
+  user: Usuario = new Usuario()
+  confirmarSenha: string
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    window.scroll(0, 0)
+
   }
 
+  confirmeSenha(event: any){
+    this.confirmarSenha = event.target.value
+  }
+
+  cadastrar(){
+    
+    if(this.user.nomeCompleto.length < 5){
+      alert('O usuário deve conter no mínimo 5 caracteres.')
+    }
+    if(this.user.usuario.indexOf('@') == -1 || this.user.usuario.indexOf('.') == -1){
+      alert('O usuário deve ser um email (e.g. usuario@usuario.com)')
+    }
+   
+
+    if(this.user.senha.length < 8){
+      alert('A senha deve conter no mínimo 8 dígitos.')
+    }else if(this.user.senha != this.confirmarSenha){
+      alert('As senhas informadas estão diferentes!')
+    }else{
+      this.authService.cadastrar(this.user).subscribe((resp: Usuario) => {
+        this.user = resp
+        this.router.navigate(['/home'])
+        alert('Usuário cadastrado com sucesso')
+      })
+    }
+  }
 }
